715863278f533d45a96dd13418a1b79ad8d83fbb
Batuhan Topçu <topcubatuhan00@hotmail.com>
2024-06-22 15:33:08 +0300

    counter added

---
+++ AgencyVilla/ViewComponents/UILayout/_DefaultCounter.cs
@@ -0,0 +1,25 @@
+﻿using AgencyVilla.Business.Abstract;
+using AgencyVilla.Dto.Dtos.CounterDtos;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AgencyVilla.ViewComponents.UILayout;
+
+public class _DefaultCounter : ViewComponent
+{
+    private readonly ICounterService _counterService;
+    private readonly IMapper _mapper;
+
+    public _DefaultCounter(ICounterService counterService, IMapper mapper)
+    {
+        _counterService = counterService;
+        _mapper = mapper;
+    }
+
+    public async Task<IViewComponentResult> InvokeAsync()
+    {
+        var data = await _counterService.TGetListAsync();
+        var mapped = _mapper.Map<List<ResultCounterDto>>(data);
+        return View(mapped);
+    }
+}
---
+++ AgencyVilla/ViewComponents/UILayout/_DefaultVideo.cs
@@ -0,0 +1,25 @@
+﻿using AgencyVilla.Business.Abstract;
+using AgencyVilla.Dto.Dtos.VideoDtos;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AgencyVilla.ViewComponents.UILayout;
+
+public class _DefaultVideo : ViewComponent
+{
+    private readonly IVideoService _videoService;
+    private readonly IMapper _mapper;
+
+    public _DefaultVideo(IVideoService videoService, IMapper mapper)
+    {
+        _videoService = videoService;
+        _mapper = mapper;
+    }
+
+    public async Task<IViewComponentResult> InvokeAsync()
+    {
+        var data = await _videoService.TGetListAsync();
+        var mapped = _mapper.Map<List<ResultVideoDto>>(data);
+        return View(mapped);
+    }
+}
